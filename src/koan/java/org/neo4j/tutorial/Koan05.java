09162f6ce00ecee40a0cca15023d147719872c02
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2013-05-13 21:00:16 +0200

    neo4j-tutorial, fix koan05

--- src/koan/java/org/neo4j/tutorial/Koan05.java
+++ src/koan/java/org/neo4j/tutorial/Koan05.java
@@ -1,12 +1,5 @@
 package org.neo4j.tutorial;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.neo4j.tutorial.matchers.ContainsOnlyHumanCompanions.containsOnlyHumanCompanions;
-import static org.neo4j.tutorial.matchers.ContainsOnlySpecificTitles.containsOnlyTitles;
-
-import java.util.HashSet;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,6 +7,13 @@ import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.index.Index;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.neo4j.tutorial.matchers.ContainsOnlyHumanCompanions.containsOnlyHumanCompanions;
+import static org.neo4j.tutorial.matchers.ContainsOnlySpecificTitles.containsOnlyTitles;
 
 /**
  * In this Koan we start to mix indexing and core API to perform more targeted
@@ -47,8 +47,19 @@ public class Koan05
         int numberOfRegenerations = 1;
 
         // YOUR CODE GOES HERE
+        Node firstDoctor = actorsIndex.get( "actor", "William Hartnell" ).getSingle();
 
-        assertEquals( 11, numberOfRegenerations );
+        Node regeneration = firstDoctor;
+
+
+        do {
+            Relationship rel = regeneration.getSingleRelationship(DoctorWhoRelationships.REGENERATED_TO, Direction.OUTGOING);
+            regeneration = rel == null ? null : rel.getEndNode();
+            numberOfRegenerations = regeneration == null ? numberOfRegenerations : numberOfRegenerations+1;
+        } while (regeneration != null);
+
+
+            assertEquals(11, numberOfRegenerations);
     }
 
     @Test
@@ -57,6 +68,21 @@ public class Koan05
         HashSet<Node> humanCompanions = new HashSet<Node>();
 
         // YOUR CODE GOES HERE
+        Node human = universe.getDatabase()
+                .index()
+                .forNodes( "species" )
+                .get( "species", "Human" )
+                .getSingle();
+
+        for (Relationship companionRel : universe.theDoctor().getRelationships(Direction.INCOMING, DoctorWhoRelationships.COMPANION_OF)) {
+            Node companionNode = companionRel.getStartNode();
+            for (Relationship isaRel : companionNode.getRelationships(Direction.OUTGOING, DoctorWhoRelationships.IS_A)) {
+                if (human.equals(isaRel.getEndNode())) {
+                    humanCompanions.add(companionNode);
+                    break;
+                }
+            }
+        }
 
         int numberOfKnownHumanCompanions = 40;
         assertEquals( numberOfKnownHumanCompanions, humanCompanions.size() );
@@ -75,10 +101,23 @@ public class Koan05
         HashSet<Node> episodesWhereRoseFightsTheDaleks = new HashSet<Node>();
 
         // YOUR CODE GOES HERE
+        Node rose = friendliesIndex.get("character", "Rose Tyler").getSingle();
+        Node dalek = speciesIndex.get("species", "Dalek").getSingle();
+
+        for (Relationship relationship : rose.getRelationships(Direction.OUTGOING, DoctorWhoRelationships.APPEARED_IN)) {
+            episodesWhereRoseFightsTheDaleks.add(relationship.getEndNode());
+        }
+
+        HashSet<Node> episodesWithDaleks = new HashSet<Node>();
+        for (Relationship relationship : dalek.getRelationships(Direction.OUTGOING, DoctorWhoRelationships.APPEARED_IN)) {
+            episodesWithDaleks.add(relationship.getEndNode());
+        }
+
+        episodesWhereRoseFightsTheDaleks.retainAll(episodesWithDaleks);
 
         assertThat(
                 episodesWhereRoseFightsTheDaleks,
-                containsOnlyTitles( "Army of Ghosts", "The Stolen Earth", "Doomsday", "Journey's End", "Bad Wolf",
-                        "The Parting of the Ways", "Dalek" ) );
+                containsOnlyTitles("Army of Ghosts", "The Stolen Earth", "Doomsday", "Journey's End", "Bad Wolf",
+                        "The Parting of the Ways", "Dalek"));
     }
 }
